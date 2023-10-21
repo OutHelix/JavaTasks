e1e02c91cad0a03ee27ccf629e49122b9e513bf7
OutHelix <combo.contra@gmail.com>
2023-10-21 22:12:36 +0300

    some changes

--- src/task3.java
+++ src/task3.java
@@ -73,7 +73,6 @@ public class task3 {
             if (shops.length == maxShops) {
                 products.add(shops[0]);
             }
-            if (shops.length == maxShops) {products.add(shops[0]);}
         }
         return products;
     }
